c3d2b2c3d2c3fa661513c2e8d7740ffbb08fd80f
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-02-26 04:57:14 +0100

    recdebits done

--- src/components/RecurringDebits.tsx
+++ src/components/RecurringDebits.tsx
@@ -15,13 +15,35 @@ const formatAmount = (amount : number) : string => {
     return amount >= 0 ? '$'+amount.toFixed(2) : '-$'+amount.toFixed(2).toString().slice(1)
 }
 
+interface DebitType {
+    company: string
+    amount: string
+}
+
 const RecurringDebits = () => {
 
     return (
         <div className='recDebitsContainer'>
             <h2 className="debitsTitle">Recurring Debits</h2>
-
+            {DebitsList.map((debit, index) => 
+            <Debit 
+                key={'debit'+index} 
+                company={debit.company.toUpperCase()} 
+                amount={formatAmount(debit.amount)}/>)}
+            <div className='grid16'></div>
         </div>
+    )
+}
+
+const Debit = ({company, amount} : DebitType) => {
+    return(
+        <article className="oneDebitContainer">
+            <div className='companynAmountContainer'>
+                <span className='debitRecipient'>{company}</span>
+                <span className={amount[0]!=='-' ? 'debitAmount' : 'debitAmountRed'}>{amount}</span>
+            </div>
+            <div className='debitDashes'></div>
+        </article>
     )
 }
 
