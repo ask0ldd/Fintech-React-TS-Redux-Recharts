9aaf34dc1d956a626284ac38539eac51b7100190
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-02-27 04:24:58 +0100

    spacing improved

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -12,11 +12,13 @@ const Balance = () => {
                 <span className='balance__amount'>$3601</span>
                 <div className='balance__select'><span className='balance__percentage'>+10% (30 days)</span><div className='balance__selectArrows'></div></div>
             </article>
+            <div className='balance__separator'></div>
             <article className='balance__column'>
                 <span className='balance__title'>Income</span>
                 <span className='balance__amount'>$1601</span>
                 <div className='balance__select'><span className='balance__percentage'>+10% (30 days)</span><div className='balance__selectArrows'></div></div>
             </article>
+            <div className='balance__separator'></div>
             <article className='balance__column'>
                 <span className='balance__title'>Expenses</span>
                 <span className='balance__amount'>$901</span>
