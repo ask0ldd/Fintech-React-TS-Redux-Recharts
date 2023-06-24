c212c39b5e940eaa8b391ff1d59e79f89e8855de
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-25 01:00:09 +0200

    vmenu update + body width

--- src/components/VMenu.tsx
+++ src/components/VMenu.tsx
@@ -3,6 +3,10 @@ import '../styles/VMenu.css'
 function VMenu(){
     return(
         <div className='vmenu'>
+            <div role="button" className='homeButton'>
+                <img src="/menu_icons/home.svg"/>
+            </div>
+            <img src="/menu_icons/settings.png"/>
             <img src="/menu_icons/accounts.png"/>
             <img src="/menu_icons/chat.png"/>
             <img src="/menu_icons/settings.png"/>
