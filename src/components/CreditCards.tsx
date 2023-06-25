226d3c1d26e3ad3c4b9d7692064262078c4f9e98
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-25 05:03:53 +0200

    credit cards col right dispose missing

--- src/components/CreditCards.tsx
+++ src/components/CreditCards.tsx
@@ -1,4 +1,5 @@
 import creditcards from '/creditcards.png'
+import qrcode from '/qrcode.png'
 import '../styles/CreditCards.css'
 
 function CreditCards(){
@@ -7,11 +8,15 @@ function CreditCards(){
             <div className='creditcardsnTitle__container'>
                 <h2 className='creditcards__title'>Credit Cards</h2>
                 <img className='creditcards' src={creditcards}/>
-                <div className='creditcards__rightContainer'>
-                    <div>Expires in : 4 min 57</div>
-                </div>
             </div>
             <div className='separator'></div>
+            <div className='creditcards__rightContainer'>
+                <div className='expiration__container'>Expires in : <span>4 min 57</span></div>
+                <div className='qrcode__container'>
+                    <div>aaa</div>
+                    <img src={qrcode}/>
+                </div>
+            </div>
         </article>
     )
 }
