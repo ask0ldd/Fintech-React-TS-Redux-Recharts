fa50053efb6ea368a532df2051c9a29424878fdb
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-25 18:54:13 +0200

    creditcard options

--- src/components/CreditCards.tsx
+++ src/components/CreditCards.tsx
@@ -3,26 +3,36 @@ import qrcode from '/qrcode.png'
 import dispose from '/qrcode_icons/dispose.png'
 import refresh from '/qrcode_icons/refresh.png'
 import '../styles/CreditCards.css'
+import CreditCardsOptions from './CreditCardsOptions'
 
 function CreditCards(){
     return (
-        <article className='creditcards__container'>
-            <div className='creditcardsnTitle__container'>
-                <h2 className='creditcards__title'>Credit Cards</h2>
-                <img className='creditcards' src={creditcards}/>
-            </div>
-            <div className='separator'></div>
-            <div className='creditcards__rightContainer'>
-                <div className='expiration__container'>Expires in : <span>4 min 57</span></div>
-                <div className='qrcode__container'>
-                    <div className='qrcodeicons__container'>
-                        <img className='qrcode__refresh' src={refresh}/>
-                        <img src={dispose}/>
-                    </div>
-                    <img className='qrcode' src={qrcode}/>
+        <div className='CreditcardnOptions__container'>
+            <article className='creditcards__container'>
+                <div className='creditcardsnTitle__container'>
+                    <h2 className='creditcards__title'>Credit Cards</h2>
+                    <img className='creditcards' src={creditcards}/>
                 </div>
+                <div className='separator'></div>
+                <div className='creditcards__rightContainer'>
+                    <div className='expiration__container'>Expires in : <span>4 min 57</span></div>
+                    <div className='qrcodenOptions__container'>
+                        <div className='qrcode__container'>
+                            <div className='qrcodeicons__container'>
+                                <img className='qrcode__refresh' src={refresh}/>
+                                <img src={dispose}/>
+                            </div>
+                            <img className='qrcode' src={qrcode}/>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <div className='creditcardsOptions__container'>
+                <CreditCardsOptions/>
+                <CreditCardsOptions/>
+                <CreditCardsOptions/>
             </div>
-        </article>
+        </div>
     )
 }
 
---
+++ src/components/CreditCardsOptions.tsx
@@ -0,0 +1,7 @@
+import '../styles/CreditCardsOptions.css'
+
+function CreditCardsOptions(){
+    return(<div className='creditcardOption_button'></div>)
+}
+
+export default CreditCardsOptions
