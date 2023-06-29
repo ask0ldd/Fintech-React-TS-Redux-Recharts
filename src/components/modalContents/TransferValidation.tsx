01561abd0af0296170f9e0183ea510d02af4f911
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-30 01:10:17 +0200

    modal transfer confirmation styling done

--- src/components/modalContents/TransferValidation.tsx
+++ src/components/modalContents/TransferValidation.tsx
@@ -7,15 +7,15 @@ function TransferValidation({setModalVisibility, rows, setRows} : IProps){
         <h2>Transfer Confirmation</h2>
         <h3>Transfer Summary</h3>
         <div className="transferSummary__container">
-            <img src="./avatars/tonyavatar.png"/>
-            <div style={{display:'flex', justifyContent:'center', flexDirection:'column'}}>
-                <span>Recipient</span>
-                <span>Recipient</span>
-                <span>Recipient</span>
-                <span>Recipient</span>
+            <img className="transferAvatar" src="./avatars/tonyavatar.png"/>
+            <div style={{display:'flex', justifyContent:'center', flexDirection:'column', rowGap:'6px'}}>
+                <span className="boldPoppins">Recipient</span>
+                <span className="textJost">Annette Black</span>
+                <span className="boldPoppins">Amount</span>
+                <span className="textJost">$300</span>
             </div>
         </div>
-        <h3>Estimated Date</h3>
+        <h3 style={{marginTop:'4px'}}>Estimated Date</h3>
         <div className="estimatedDate__container">
             24/06/2023
         </div>
