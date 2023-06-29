e22fc3de113bcb7ed99615626b4e7a7b729fa87a
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-06-30 00:10:57 +0200

    modal transfer confirmation layout ok

--- src/components/modalContents/TransferValidation.tsx
+++ src/components/modalContents/TransferValidation.tsx
@@ -4,9 +4,20 @@ import '../../styles/TransferValidation.css'
 function TransferValidation({setModalVisibility, rows, setRows} : IProps){
  return(
     <div className='transferValidation__bodyContainer'>
-        <h2>Transfer Summary</h2>
+        <h2>Transfer Confirmation</h2>
+        <h3>Transfer Summary</h3>
         <div className="transferSummary__container">
-            aaa
+            <img src="./avatars/tonyavatar.png"/>
+            <div style={{display:'flex', justifyContent:'center', flexDirection:'column'}}>
+                <span>Recipient</span>
+                <span>Recipient</span>
+                <span>Recipient</span>
+                <span>Recipient</span>
+            </div>
+        </div>
+        <h3>Estimated Date</h3>
+        <div className="estimatedDate__container">
+            24/06/2023
         </div>
         <button className='confirmSelection__button' onClick={() => setModalVisibility(false)}>Confirm This Transfer</button>
     </div>
