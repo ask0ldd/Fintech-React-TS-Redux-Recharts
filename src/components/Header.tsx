3b6fa5f9c07ae20edfdd1d55837f6044c21195bc
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-01 23:21:38 +0200

    img links upd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,23 @@
 import '../styles/Header.css'
+import bankLogo from '/banklogo.png'
+import logoutIcon from '/buttons/logout2.png'
+import tonyAvatar from '/avatars/tonyavatar.png'
 
 function Header(){
     return (
         <section className="header__container">
-            <img src='/avatars/tonyavatar.png'/>
+            <img src={tonyAvatar}/>
             <div className="namenIban__container">
                 <span className="name">Tony Montana</span>
                 <span className="iban">IBAN : NL89RABO1289364745</span>
             </div>
             <figure className='banklogo__container'>
-                <img src='/banklogo.png'/>
+                <img src={bankLogo}/>
             </figure>
             <div className='verticalSeparator'>
             </div>
             <div className='logout__container'>
-                <img role="button" src='/buttons/logout2.png'/>
+                <img role="button" src={logoutIcon}/>
                 <span>LOG OUT</span>
             </div>
         </section>
--- src/components/VMenu.tsx
+++ src/components/VMenu.tsx
@@ -3,6 +3,7 @@ import settingsIcon from '/menu_icons/settings.png'
 import homeIcon from '/menu_icons/home.svg'
 import accountsIcon from '/menu_icons/accounts.png'
 import chatIcon from '/menu_icons/chat.png'
+import statsIcon from '/menu_icons/stats.png'
 import darkmodeIcon from '/menu_icons/darkmode.png'
 import { Link } from 'react-router-dom'
 
@@ -10,13 +11,13 @@ function VMenu({activePage} : IProps){
     return(
         <div className='vmenu'>
             <Link to="/" className='homeButton'>
-                <img src='/menu_icons/home.svg'/>
+                <img src={homeIcon}/>
             </Link>
-            <Link to="/stats/balance" className='vmenu-anchor'><img src='/menu_icons/stats.png'/></Link>
-            <img src='/menu_icons/accounts.png'/>
-            <img src='/menu_icons/chat.png'/>
-            <img src='/menu_icons/settings.png'/>
-            <img src='/menu_icons/darkmode.png'/>
+            <Link to="/stats/balance" className='vmenu-anchor'><img src={statsIcon}/></Link>
+            <img src={accountsIcon}/>
+            <img src={chatIcon}/>
+            <img src={settingsIcon}/>
+            <img src={darkmodeIcon}/>
         </div>
     )
 }
@@ -24,5 +25,5 @@ function VMenu({activePage} : IProps){
 export default VMenu
 
 interface IProps{
-    activePage: string
+    activePage?: string
 }
