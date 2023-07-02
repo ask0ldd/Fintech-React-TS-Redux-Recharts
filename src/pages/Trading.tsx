79da60b5b4356a51cbe461502283d24fd90f2243
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-02 21:50:28 +0200

    trading page base

---
+++ src/pages/Trading.tsx
@@ -0,0 +1,11 @@
+import VMenu from "../components/VMenu"
+
+function Trading(){
+    return(
+        <div className="tradingPage__mainContainer">
+            <VMenu activePage="trading" />
+        </div>
+    )
+}
+
+export default Trading
