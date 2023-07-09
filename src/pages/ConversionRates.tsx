8fdc19d44d8645f580a9fba56dc96ebbc3479605
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-09 06:57:25 +0200

    conversion page skeleton

--- src/pages/ConversionRates.tsx
+++ src/pages/ConversionRates.tsx
@@ -1,3 +1,4 @@
+import Header from '../components/Header'
 import VMenu from '../components/VMenu'
 import '../styles/ConversionRates.css'
 
@@ -5,6 +6,18 @@ function ConversionRates(){
     return (
         <div className="conversionPage__mainContainer">
             <VMenu activePage="conversion" />
+            <div className="right__container">
+                <Header/>
+                <div className='conversionItemsRow__container'>
+                    <div className='conversion__items'></div><div className='conversion__items'></div><div className='conversion__items'></div>
+                </div>
+                <div className='conversionItemsRow__container'>
+                    <div className='conversion__items'></div><div className='conversion__items'></div><div className='conversion__items'></div>
+                </div>
+                <div className='conversionGraphnList__container'>
+                    
+                </div>
+            </div>
         </div>
     )
 }
