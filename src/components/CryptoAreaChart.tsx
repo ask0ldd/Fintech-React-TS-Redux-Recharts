d75c92df65f8c2e4a6aab8b03c0abbfda5a4e600
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-10 03:16:14 +0200

    crypto area chart : done

--- src/components/CryptoAreaChart.tsx
+++ src/components/CryptoAreaChart.tsx
@@ -2,21 +2,36 @@ import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YA
 import '../styles/CryptoAreaChart.css'
 
 const data = [
-    {week: 'week 1', price: 12400},
-    {week: 'week 2', price: 17300},
-    {week: 'week 3', price: 12700},
-    {week: 'week 4', price: 19630},
+    {week: 'w1', price: 12400},
+    {week: 'w2', price: 17300},
+    {week: 'w3', price: 12700},
+    {week: 'w4', price: 19630},
 ]
+
+function CustomYTicks(value : number) : string{
+    return(
+        (value/1000).toString()+'k'
+    )
+}
 
 function CryptoAreaChart(){
     return(
         <ResponsiveContainer className="cryptoareaChart__container" width="40%" height="100%">
-            <AreaChart width={300} height={800} data={data} margin={{top: 10, right: 30, left: 0, bottom: 0,}}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="week" />
-                <YAxis />
+            <AreaChart width={300} data={data} margin={{top: 64, right: 32, left: 8, bottom: 48,}}>
+                <defs>
+                    <linearGradient id="PurpleUV" x1="0" y1="1" x2="0" y2="0" /* Line Gradient Definition */>
+                        <stop offset="0%" stopColor="#8B3FAF" stopOpacity={0.47} />
+                        <stop offset="100%" stopColor="#703FAF" stopOpacity={1} />
+                    </linearGradient>
+                </defs>
+                <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#8397AF"/>
+                <XAxis dataKey="week" tickMargin={14}/>
+                <YAxis domain={[5000, 20000]} tickCount={4} tickFormatter={CustomYTicks} tickMargin={8} interval="preserveStartEnd"/>
                 <Tooltip />
-                <Area type="monotone" dataKey="price" stroke="#8884d8" fill="#8884d8" />         
+                <Area type="monotone" dataKey="price" stroke="#441B95" fill="url(#PurpleUV)" />
+                <text textAnchor="start" fontSize={26} fontWeight={700} x={29} y={28} fontFamily="Poppins" fill="#5c39aa">
+                    BTC Value
+                </text>    
             </AreaChart>
         </ResponsiveContainer>
     )
--- src/components/OwnedCurrencyList.tsx
+++ src/components/OwnedCurrencyList.tsx
@@ -17,7 +17,7 @@ function OwnedCurrencyList(){
         <div className='ownedCurrencyList__container'>
             <h2>Your Wallet</h2>
             <table>
-                <thead><tr><th style={{textAlign:'left', width:'60px', paddingLeft:'0.75rem'}}>Currency</th><th>Amount</th><th>Value (USD)</th><th>Price</th><th style={{paddingRight:'0.75rem'}}>Last 30d</th></tr></thead>
+                <thead><tr><th style={{textAlign:'left', width:'60px', paddingLeft:'0.745rem'}}>Currency</th><th>Amount</th><th>Value (USD)</th><th>Price</th><th style={{paddingRight:'0.75rem'}}>Last 30d</th></tr></thead>
                 <tbody>
                     {[...wallet].map((array, index) => {const priceVar = ((array.price / array.previousPrice - 1) * 100).toFixed(2); return(<tr style={wallet.length-1 === index ? {borderBottom:'1px solid rgba(65, 65, 65, 0.9)'} : {}}className={ index%2 === 0 ? 'ownedCurrencyList__rowEven' : ''} key={'walletRow'+index}><td style={{textAlign:'left', paddingLeft:'0.75rem', color:'#5C39AA', fontFamily:'Poppins', fontWeight:'700'}}>{array.currency}</td><td>{array.amount.toFixed(2)}</td><td>{(array.price * array.amount).toFixed(2)}</td><td>{array.price.toFixed(2)}</td><td className={parseFloat(priceVar) >= 0 ? 'ownedCurrencyList__inc' : ''} style={{paddingRight:'0.75rem'}}>{parseFloat(priceVar) >= 0 ? '+'+priceVar : '-'+priceVar}</td></tr>)})}
                 </tbody>
