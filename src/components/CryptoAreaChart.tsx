9a8f21ef5c28f25d616202a7532ecd4a7cc91f5e
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-10 04:36:56 +0200

    custom tooltip crypto area chart

--- src/components/CryptoAreaChart.tsx
+++ src/components/CryptoAreaChart.tsx
@@ -17,19 +17,25 @@ function CustomYTicks(value : number) : string{
 function CryptoAreaChart(){
     return(
         <ResponsiveContainer className="cryptoareaChart__container" width="40%" height="100%">
-            <AreaChart width={300} data={data} margin={{top: 64, right: 26, left: 8, bottom: 48,}}>
+            <AreaChart width={300} data={data} margin={{top: 64, right: 38, left: 4, bottom: 48,}}>
                 <defs>
                     <linearGradient id="PurpleUV" x1="0" y1="1" x2="0" y2="0" /* Line Gradient Definition */>
                         <stop offset="0%" stopColor="#8B3FAF" stopOpacity={0.47} />
                         <stop offset="100%" stopColor="#703FAF" stopOpacity={1} />
                     </linearGradient>
                 </defs>
-                <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#8397AF"/>
-                <XAxis dataKey="week" tickMargin={14}/>
-                <YAxis domain={[5000, 20000]} tickCount={4} tickFormatter={CustomYTicks} tickMargin={14} interval="preserveStartEnd"/>
-                <Tooltip />
-                <Area type="monotone" dataKey="price" stroke="#441B95" fill="url(#PurpleUV)" />
-                <text textAnchor="start" fontSize={26} fontWeight={700} x={24} y={28} fontFamily="Poppins" fill="#5c39aa">
+                <CartesianGrid strokeDasharray="3 3" stroke="#8397AF"/>
+                <XAxis dataKey="week" fontSize={14} tickMargin={14} fill="#000000"/>
+                <YAxis domain={[5000, 20000]} fontSize={14} fill="#000000" tickCount={4} tickFormatter={CustomYTicks} tickMargin={16} interval="preserveStartEnd"/>
+                <Tooltip 
+                    cursor={false}
+                    wrapperStyle={{outline:'none', border:'none'}}
+                    itemStyle={{color:'#000', fontSize:'10px'}}
+                    labelStyle={{color:'#000',display:'none'}}
+                    content={CustomTooltip}
+                />
+                <Area type="monotone" dataKey="price" strokeWidth={2} stroke="#441B95" fill="url(#PurpleUV)" />
+                <text textAnchor="start" fontSize={26} fontWeight={700} x={14} y={28} fontFamily="Poppins" fill="#5c39aa">
                     BTC Value
                 </text>    
             </AreaChart>
@@ -38,3 +44,15 @@ function CryptoAreaChart(){
 }
 
 export default CryptoAreaChart
+
+const CustomTooltip = ({payload} : any) => {
+    if(payload[0] && payload.length){
+        console.log(payload[0].payload.week)
+        return(
+            <div className="areachart__tooltip">
+                <p>Week : {payload[0].payload.week[1]}</p>
+                <p style={{marginTop:'4px'}}>Price : {payload[0].payload.price} USD</p>
+            </div>
+        )
+    }
+}
