f232d23ababa54540039e6cd4b73d1f8268af8ec
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-12 02:41:52 +0200

    overall app styling improvements

--- src/pages/Trading.tsx
+++ src/pages/Trading.tsx
@@ -15,14 +15,22 @@ function Trading(){
                         <h3 style={{marginTop:'2px'}}>Crypto Balance (7d)</h3>
                         <p style={{marginTop:'12px'}} className="mainText">12,025 USD (prev. 8,737 USD)</p>
                     </div>
-                    <div className='trading__items'></div>
                     <div className='trading__items'>
                         <div className="trading__items__textContainer">
-                            <h3 style={{marginTop:'2px'}}>Top Performer (7d)</h3>
+                            <h3 style={{marginTop:'2px'}}>Top Currency Perf. (7d)</h3>
+                            <p style={{marginTop:'12px'}} className="mainText">CHF / Swiss Franc</p>
+                        </div>
+                        <div className="trading__items__button">
+                            +2.6%
+                        </div>
+                    </div>
+                    <div className='trading__items'>
+                        <div className="trading__items__textContainer">
+                            <h3 style={{marginTop:'2px'}}>Top Crypto Perf. (7d)</h3>
                             <p style={{marginTop:'12px'}} className="mainText">ETH / Ethereum</p>
                         </div>
                         <div className="trading__items__button">
-                            -8.3%
+                            +8.3%
                         </div>
                     </div>
                 </div>
@@ -31,10 +39,18 @@ function Trading(){
                         <h3 style={{marginTop:'2px'}}>Crypto Balance (7d)</h3>
                         <p style={{marginTop:'12px'}} className="mainText">12,025 USD (prev. 8,737 USD)</p>
                     </div>
-                    <div className='trading__items'></div>
                     <div className='trading__items'>
                         <div className="trading__items__textContainer">
-                            <h3 style={{marginTop:'4px'}}>Worst Performer (7d)</h3>
+                            <h3 style={{marginTop:'4px'}}>Worst Currency Perf. (7d)</h3>
+                            <p style={{marginTop:'10px'}} className="mainText">APS / Argentine Peso</p>
+                        </div>
+                        <div className="trading__items__button">
+                            -5.1%
+                        </div>
+                    </div>
+                    <div className='trading__items'>
+                        <div className="trading__items__textContainer">
+                            <h3 style={{marginTop:'4px'}}>Worst Crypto Perf. (7d)</h3>
                             <p style={{marginTop:'10px'}} className="mainText">BIN / Binance</p>
                         </div>
                         <div className="trading__items__button">
