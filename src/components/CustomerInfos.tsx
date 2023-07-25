54a919c529f3386d8acf4031a25d6673238879dc
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-25 03:22:27 +0200

    settings page : customerInfo two cols basis

--- src/components/CustomerInfos.tsx
+++ src/components/CustomerInfos.tsx
@@ -4,14 +4,13 @@ function CustomerInfos(){
     return(
         <article className='customerInfos__container'>
             <h2>About You</h2>
-            <ul>
-                <li className='label'>Your Center :</li>
-                <li className='value'>Bank Of America OC</li>
-                <li className='label'>Address :</li>
-                <li className='value'>2680 N Tustin St, Orange</li>
-                <li className='label'>State :</li>
-                <li className='value'>California 92865</li>
-            </ul>
+            <div className='infosTable__container'>
+                <div className='labelRow'>Your Center :</div>
+                <div className='valueRow'>Bank Of America OC</div>
+                <div className='labelRow'>Address :</div>
+                <div className='valueRow'>2680 N Tustin St, Orange</div>
+
+            </div>
         </article>
     )
 }
