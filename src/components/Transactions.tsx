1a17e56fa219980314a47ea1fe18284930c382fc
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-26 08:27:58 +0200

    wallet hover

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -2,8 +2,11 @@ import '../styles/Transactions.css'
 import { Formatter } from '../services/formatter'
 import { ITransactionType } from '../types/types'
 import { TransactionsList } from '../datas/datas'
+import { useState } from 'react'
 
 const Transactions = () => {
+
+    const [isTextDelayed, setTextHasDelayed] = useState<boolean>(false)
 
     return (
         <article className="transactions__container">
@@ -27,7 +30,7 @@ const Transaction = ({company, date, amount} : ITransactionType) => {
     return(
         <article className="oneTransaction__container">
             <div className='transaction__datas'>
-                <div className='transaction__recipient'><span className='delayed__text'>{company}</span><div className='default__loading loading__mgp2'></div></div>
+                <div className='transaction__recipient'><span className={'delayed__text'}>{company}</span><div className='default__loading loading__mgp2'></div></div>
                 <div className='datenAmount__container'>
                     <span className='transaction__date delayed__text'>{date}</span><span className={amount >= 0 ? 'transaction__amount delayed__text' : 'transaction__amountRed delayed__text'}>{formatedAmount}</span><div className='default__loading'></div>
                 </div>
