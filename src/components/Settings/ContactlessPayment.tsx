7b8d64f4533f6a2b7d7691a0d90c7069500197b0
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-26 04:40:46 +0200

    contactless payment ssitch anim ok

--- src/components/Settings/ContactlessPayment.tsx
+++ src/components/Settings/ContactlessPayment.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import '../../styles/ContactlessPayment.css'
 
 function ContactlessPayment(){
+
+    const [isSwitchActive, setSwitchActive] = useState<boolean>(true)
+
     return(
         <article className='contactlessPayment__container'>
             <div className='creditCardInfosnTitle__container'>
@@ -9,10 +13,10 @@ function ContactlessPayment(){
                 <span className='expirationDate'>Expiration Date : <span className='expirationDate__value'>12/2026</span></span>
             </div>
             <div className='contactlessActivation__container'>
-                <span className='onOff__label'>On</span>
-                <div className='switchContainer'>
-                    <div className='switch' aria-role='button'>
-                        
+                <span className='onOff__label'>{isSwitchActive ? 'On' : 'Off'}</span>
+                <div className='switchContainer' onClick={() => setSwitchActive(!isSwitchActive)}>
+                    <div className={isSwitchActive ? 'switch' : 'deactivatedSwitch'} style={{/*right: isSwitchActive ? '0px' : '34px'*/}}>
+
                     </div>
                 </div>
             </div>
