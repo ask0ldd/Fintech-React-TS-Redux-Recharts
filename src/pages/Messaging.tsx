0c3e6f40babab08d3b7ca1766f24a273c0fe8b72
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-31 22:15:30 +0200

    balance perc align

--- src/pages/Messaging.tsx
+++ src/pages/Messaging.tsx
@@ -19,6 +19,7 @@ function Messaging(){
                 <Header format="uncompressed" username="Tony Montana" iban="NL89RABO1289364745" clientID='X458-89995'/>
                 { activeMessagingSection === 'inbox' && <><MessagingHorizontalMenu activeMessagingSection="inbox"/><InboxTable/></>}
                 { activeMessagingSection === 'newmail' && <><MessagingHorizontalMenu activeMessagingSection="newmail"/><NewMessage/></>}
+                { activeMessagingSection === 'sent' && <><MessagingHorizontalMenu activeMessagingSection="sent"/><InboxTable/></>}
             </section>
 
         </main>
