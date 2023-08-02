304f88a84a2414fa2e2e4c6ed083e300aaa5330a
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-02 21:31:20 +0200

    inbox menu base

--- src/components/Messaging/ContactsList.tsx
+++ src/components/Messaging/ContactsList.tsx
@@ -5,7 +5,7 @@ function ContactsList(){
         <article className="contactsList__container">
             <h2 style={{marginBottom:'1.15rem',}}>Contacts</h2>
             <div className='contact__container'>
-                *
+                <figure style={{marginTop:'6px'}}>*</figure>
                 <img src="../avatars/avatar4.png"/>
                 <div>
                     <p style={{fontWeight:'600'}}>Jimmy Marklum</p>
@@ -13,7 +13,7 @@ function ContactsList(){
                 </div>
             </div>
             <div className='contact__container'>
-                *
+                <figure style={{marginTop:'6px'}}>*</figure>
                 <img src="../avatars/avatar5.png"/>
                 <div>
                     <p style={{fontWeight:'600'}}>Sonya Poniatosky</p>
@@ -21,7 +21,7 @@ function ContactsList(){
                 </div>
             </div>
             <div className='contact__container'>
-                *
+                <figure style={{marginTop:'6px'}}>*</figure>
                 <img src="../avatars/avatar8.png"/>
                 <div>
                     <p style={{fontWeight:'600'}}>Julie Brown</p>
@@ -29,7 +29,7 @@ function ContactsList(){
                 </div>
             </div>
             <div className='contact__container'>
-                *
+                <figure style={{marginTop:'6px'}}>*</figure>
                 <img src="../avatars/avatar7.png"/>
                 <div>
                     <p style={{fontWeight:'600'}}>Aria Delico</p>
@@ -37,7 +37,7 @@ function ContactsList(){
                 </div>
             </div>
             <div className='contact__container'>
-                *
+                <figure style={{marginTop:'6px'}}>*</figure>
                 <img src="../avatars/avatar3.png"/>
                 <div>
                     <p style={{fontWeight:'600'}}>Minnie Steinbeck</p>
---
+++ src/components/Messaging/VInboxMenu.tsx
@@ -0,0 +1,8 @@
+function VInboxMenu(){
+    return(
+        <article className="vinboxMenu__container">
+        </article>
+    )
+}
+
+export default VInboxMenu
