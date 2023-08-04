613bef0d6e88008404215491ced7a38b102aafa6
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-04 14:44:14 +0200

    delete selected emails fn

--- src/pages/Messaging.tsx
+++ src/pages/Messaging.tsx
@@ -19,15 +19,35 @@ function Messaging(){
     const activeMessagingSection : string = useParams().id || "inbox"
 
     const [mailRecipient, setMailRecipient] = useState<IRecipient>({name : 'Jimmy Marklum', pic : './avatars/blank.png', title : 'Bank Advisor'})
+
+    function deleteSelectedEmails(){
+        const nonDeletedEmails = [...emailsState].filter(email => email.selected === false)
+        setEmailsState(nonDeletedEmails)
+    }
     
     return(
         <main className="messagingPage__mainContainer">
             <VMenu activePage="messaging" />
             <section className="headernMessagesBox__container">
                 <Header format="uncompressed" username="Tony Montana" iban="NL89RABO1289364745" clientID='X458-89995'/>
-                { activeMessagingSection === 'inbox' && <><MessagingHorizontalMenu activeMessagingSection="inbox"/><div style={{display:'flex', flexDirection:'row', width:'100%', columnGap:'24px'}}><InboxTable emailsState={emailsState} setEmailsState={setEmailsState}/><VInboxMenu emailsState={emailsState} setEmailsState={setEmailsState}/></div></>}
-                { activeMessagingSection === 'newmail' && <><MessagingHorizontalMenu activeMessagingSection="newmail"/><div style={{display:'flex', flexDirection:'row', width:'100%', columnGap:'24px'}}><NewMessage mailRecipient={mailRecipient}/><ContactsList/></div></>}
-                { activeMessagingSection === 'sent' && <><MessagingHorizontalMenu activeMessagingSection="sent"/><InboxTable emailsState={emailsState} setEmailsState={setEmailsState}/></>}
+                { activeMessagingSection === 'inbox' && 
+                    <><MessagingHorizontalMenu activeMessagingSection="inbox"/>
+                        <div style={{display:'flex', flexDirection:'row', width:'100%', columnGap:'24px'}}>
+                            <InboxTable emailsState={emailsState} setEmailsState={setEmailsState}/>
+                            <VInboxMenu emailsState={emailsState} setEmailsState={setEmailsState} deleteSelectedEmails={deleteSelectedEmails}/>
+                        </div></>
+                }
+                { activeMessagingSection === 'newmail' && 
+                    <><MessagingHorizontalMenu activeMessagingSection="newmail"/>
+                        <div style={{display:'flex', flexDirection:'row', width:'100%', columnGap:'24px'}}>
+                            <NewMessage mailRecipient={mailRecipient}/>
+                            <ContactsList/>
+                        </div></>
+                }
+                { activeMessagingSection === 'sent' && 
+                    <><MessagingHorizontalMenu activeMessagingSection="sent"/>
+                    <InboxTable emailsState={emailsState} setEmailsState={setEmailsState}/></>
+                }
             </section>
 
         </main>
