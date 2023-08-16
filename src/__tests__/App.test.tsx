29ec8ec3ff16c9e8b3fa1e8a8aa9d33b5777c9b8
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-16 21:07:30 +0200

    confirmation modal tested

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -28,7 +28,7 @@ describe('Given I am on the Landing page', async () => {
         
     })
 
-    test('All main container components are displayed', async () => {
+    test('All main container components should be displayed', async () => {
   
         await waitFor( () => expect(screen.getByText(/Balance/i)).toBeInTheDocument())
         expect(screen.getByText(/Credit Cards/i)).toBeInTheDocument()
@@ -39,7 +39,7 @@ describe('Given I am on the Landing page', async () => {
         expect(screen.getByText(/Montana/i)).toBeInTheDocument()
     })
 
-    test('The vertical menu is displayed w/ all its items', async () => {
+    test('The vertical menu should be displayed w/ all its items', async () => {
         await waitFor( () => expect(screen.getByAltText("home menu item")).toBeInTheDocument())
         expect(screen.getByAltText("stats menu item")).toBeInTheDocument()
         expect(screen.getByAltText("chat menu item")).toBeInTheDocument()
@@ -48,12 +48,12 @@ describe('Given I am on the Landing page', async () => {
         expect(screen.getByAltText("dark mode menu item")).toBeInTheDocument()
     })
 
-    test('4 Contacts are displayed in Quick Wire Transfer Contacts List', async() => {
+    test('4 Contacts should be displayed in Quick Wire Transfer Contacts List', async() => {
         await waitFor(() => expect(screen.getAllByTestId("transferQuickContact").length).toBe(4))
         expect(screen.getAllByTestId("transferBlankContact").length).toBe(1)
     })
 
-    test('Clicking the button next to the QWT Contacts List, displays a modal', async() => {
+    test('Clicking the button next to the QWT Contacts List should open a modal', async() => {
         await waitFor(() => expect(screen.getByText(/Quick Wire Transfer/i)).toBeInTheDocument())
         expect(screen.queryByTestId("modal")).not.toBeInTheDocument() // !!! querybytestid or it will throw an error
         const addContactButton = screen.getAllByRole("button").filter(button => button.classList.contains("xButton"))[0]
@@ -69,9 +69,23 @@ describe('Given I am on the Landing page', async () => {
         })
     })
 
+    test('Clicking the button next to the QWT Amount should open a modal', async() => {
+        await waitFor(() => expect(screen.getByText(/Quick Wire Transfer/i)).toBeInTheDocument())
+        expect(screen.queryByTestId("modal")).not.toBeInTheDocument() // !!! querybytestid or it will throw an error
+        const addContactButton = screen.getAllByRole("button").filter(button => button.classList.contains("vButton"))[0]
+        
+        act(() => addContactButton.click())
+        await waitFor(() => expect(screen.getByTestId("modal")).toBeInTheDocument())
+
+        test('The modal should display a transfer confirmation', async() => {
+            expect(screen.getByText(/Transfer Confirmation/i)).toBeInTheDocument()
+        })
+    })
+
     test('onload has been called', async() => {
         window.dispatchEvent(new Event('load'))
-        await waitFor(() => expect(global.setTimeout).toHaveBeenCalled())
+        await waitFor(() => expect(window.setTimeout).toHaveBeenCalled())
+        // !!! onload not covered
     })
 
 })
