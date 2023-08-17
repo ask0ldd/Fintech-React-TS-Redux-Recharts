cde6e4cd70eab4fd8c097116b23156cba438678a
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-17 22:02:58 +0200

    vmenu test messaging

--- src/__tests__/VMenu.test.tsx
+++ src/__tests__/VMenu.test.tsx
@@ -5,6 +5,7 @@ import { expect, vi, describe, test, beforeAll, beforeEach } from 'vitest'
 import App from '../App'
 import Stats from '../pages/Stats'
 import Trading from '../pages/Trading'
+import Messaging from '../pages/Messaging'
 
 vi.mock('recharts', async () => {
     const OriginalModule = await vi.importActual<typeof import('recharts')>('recharts')
@@ -78,12 +79,32 @@ describe('Given I am one the Trading page', async () => {
         }
         render(<MockedRouter />)
         // render(<VMenu activePage="home"/>)
-        await waitFor( () => expect(screen.getByAltText("stats menu item").parentElement?.classList.contains('activeItem')).toBeTruthy())
+        await waitFor( () => expect(screen.getByAltText("accounts menu item").parentElement?.classList.contains('activeItem')).toBeTruthy())
         expect(screen.getByAltText("home menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
         expect(screen.getByAltText("chat menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
+        expect(screen.getByAltText("settings menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
+        expect(screen.getByAltText("stats menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
+        expect(screen.getByAltText("dark mode menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
+    })
+})
+
+describe('Given I am one the Messaging page', async () => {
+    test('the message icon should be highlighted by default', async () => {
+        
+        const MockedRouter = () => { 
+            return(
+                <BrowserRouter>
+                    <Messaging/>
+                </BrowserRouter>
+            )
+        }
+        render(<MockedRouter />)
+        // render(<VMenu activePage="home"/>)
+        await waitFor( () => expect(screen.getByAltText("chat menu item").parentElement?.classList.contains('activeItem')).toBeTruthy())
+        expect(screen.getByAltText("home menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
+        expect(screen.getByAltText("stats menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
         expect(screen.getByAltText("settings menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
         expect(screen.getByAltText("accounts menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
         expect(screen.getByAltText("dark mode menu item").parentElement?.classList.contains('activeItem')).toBeFalsy()
     })
 })
-
