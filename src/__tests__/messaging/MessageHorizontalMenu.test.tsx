0af97dc9b1b6716849cb0bb1ab34803dadeccdb1
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-18 04:09:18 +0200

    inbox menu item test

--- src/__tests__/messaging/MessageHorizontalMenu.test.tsx
+++ src/__tests__/messaging/MessageHorizontalMenu.test.tsx
@@ -4,10 +4,10 @@ import { expect, vi, describe, test, beforeAll, beforeEach } from 'vitest'
 import Messaging from '../../pages/Messaging'
 import MessagingHorizontalMenu from '../../components/MessagingHorizontalMenu'
 
-const MockedRouter = () => { 
+const MockedRouter = ({activeMessagingSection} : {activeMessagingSection : string}) => { 
     return(
         <BrowserRouter>
-            <MessagingHorizontalMenu activeMessagingSection="newmail"/>
+            <MessagingHorizontalMenu activeMessagingSection={activeMessagingSection}/>
         </BrowserRouter>
     )
 }
@@ -21,13 +21,17 @@ describe('Given I am on the Messaging page', async () => {
         HTMLDialogElement.prototype.close = vi.fn()
     })
 
-    beforeEach(() => {
-        render(<MockedRouter/>)        
-    })
-
     test('The "new mail" button should be active if newmail is the active page', async () => {  
+        render(<MockedRouter activeMessagingSection="newmail"/>)  
         await waitFor(() => expect(screen.getByText(/Send a New Message/i).classList.contains("messagingMenu__itemsActive")).toBeTruthy())
         expect(screen.getByText(/Your Messages/i).classList.contains("messagingMenu__itemsActive")).toBeFalsy()
+        expect(screen.getByText(/Sent Messages/i).classList.contains("messagingMenu__itemsActive")).toBeFalsy()
+    })
+
+    test('The "inbox" button should be active if inbox is the active page', async () => {  
+        render(<MockedRouter activeMessagingSection="inbox"/>)  
+        await waitFor(() => expect(screen.getByText(/Your Messages/i).classList.contains("messagingMenu__itemsActive")).toBeTruthy())
+        expect(screen.getByText(/Send a New Message/i).classList.contains("messagingMenu__itemsActive")).toBeFalsy()
         expect(screen.getByText(/Sent Messages/i).classList.contains("messagingMenu__itemsActive")).toBeFalsy()
     })
 })
