d41479151610fcfd4a4616def0e5cff60a8c1227
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-20 21:11:27 +0200

    attachment filtering test

--- src/__tests__/messaging/VInboxMenu.test.tsx
+++ src/__tests__/messaging/VInboxMenu.test.tsx
@@ -16,7 +16,7 @@ const MockedRouter = () => {
     )
 }
 
-describe('Given I am facing the inbox table', async () => { 
+describe('Given Im facing the Inbox Table', async () => { 
 
     beforeEach(() => {
         render(<MockedRouter/>) 
@@ -25,10 +25,25 @@ describe('Given I am facing the inbox table', async () => {
     test('The Inbox Menu should be displayed', async () => {
         await waitFor(() => expect(screen.getByText('From')).toBeInTheDocument())
         const allInboxPageButtons = screen.getAllByRole("button")
-        const inboxMenuButton = allInboxPageButtons.filter(button => button.parentElement?.classList.contains("vinboxMenu__container"))
-        expect(inboxMenuButton.length).toBe(4)
+        const inboxMenuButtons = allInboxPageButtons.filter(button => button.parentElement?.classList.contains("vinboxMenu__container"))
+        expect(inboxMenuButtons.length).toBe(4)
     })
 
-    
+    test('Clicking on the file attachment button should filter out all the mails with no attachment', async () => {
+        expect(screen.getByText("Jessy Trewartha")).toBeInTheDocument()
+        expect(screen.getAllByTestId("attachment icon").length).toBe(6)
+        const inboxMenuButtons = screen.getAllByRole("button")
+        const attachmentFilteringButton = inboxMenuButtons.filter( button => button.id == "attachmentFilterButton")[0]
+        expect(attachmentFilteringButton).not.toBeNull()
+        console.log('attachement :' , attachmentFilteringButton)
+        // click function not defined on a svg
+        act(() => attachmentFilteringButton.dispatchEvent(new MouseEvent('click', {bubbles: true})))
+        await waitFor(() => expect(screen.queryByAltText("Jessy Trewartha")).not.toBeInTheDocument())
+        const attachmentIcons = screen.getAllByTestId("attachment icon")
+        const inboxBody = screen.getByTestId("inboxbody")
+        expect(attachmentIcons.length === inboxBody.children.length).toBeTruthy()
+    })
+
+
 
 })
