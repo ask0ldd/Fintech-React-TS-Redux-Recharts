9242fd29d93bb8d22d8d3e13c0b1119525bfd6dd
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-20 21:52:55 +0200

    delete selected emails test

--- src/__tests__/messaging/VInboxMenu.test.tsx
+++ src/__tests__/messaging/VInboxMenu.test.tsx
@@ -36,7 +36,7 @@ describe('Given Im facing the Inbox Table', async () => {
         const attachmentFilteringButton = inboxMenuButtons.filter( button => button.id == "attachmentFilterButton")[0]
         expect(attachmentFilteringButton).not.toBeNull()
         console.log('attachement :' , attachmentFilteringButton)
-        // click function not defined on a svg
+        // [!] click function not defined on a svg proto > use dispatchEvent instead
         act(() => attachmentFilteringButton.dispatchEvent(new MouseEvent('click', {bubbles: true})))
         await waitFor(() => expect(screen.queryByAltText("Jessy Trewartha")).not.toBeInTheDocument())
         const attachmentIcons = screen.getAllByTestId("attachment icon")
@@ -44,6 +44,32 @@ describe('Given Im facing the Inbox Table', async () => {
         expect(attachmentIcons.length === inboxBody.children.length).toBeTruthy()
     })
 
+    test('Clicking on the delete mail button without any selected email should has no effect', async () => {
+        const inboxMenuButtons = screen.getAllByRole("button")
+        const deleteMailButton = inboxMenuButtons.filter( button => button.id == "deleteSelectedMailsButton")[0]
+        expect(deleteMailButton).not.toBeNull()
+        deleteMailButton.onclick = vi.fn((e) => deleteMailButton.onclick)
+        act(() => deleteMailButton.dispatchEvent(new MouseEvent('click', {bubbles: true})))
+        await waitFor(() => expect(deleteMailButton.onclick).toHaveBeenCalledOnce())
+        expect(screen.getByText("Bambie Petera")).toBeInTheDocument()
+        expect(screen.getByText("Lyon Grigorkin")).toBeInTheDocument()
+        expect(screen.getByText("Vincents Calvert")).toBeInTheDocument()
+        expect(screen.getByText("Lois Wedlake")).toBeInTheDocument()
+        expect(screen.getByText("Sheryl De Bernardi")).toBeInTheDocument()
+        expect(screen.getByText("Doreen Tripcony")).toBeInTheDocument()
+        expect(screen.getByText("Cicely Croxley")).toBeInTheDocument()
+        expect(screen.getByText("Theodor Vittet")).toBeInTheDocument()
+        expect(screen.getByText("Ella Lucken")).toBeInTheDocument()
+        expect(screen.getByText("Con Hasnney")).toBeInTheDocument()
+        expect(screen.getByText("Padgett Jays")).toBeInTheDocument()
+        expect(screen.getByText("Ulrikaumeko Comi")).toBeInTheDocument()
+        expect(screen.getByText("Pearline Stenning")).toBeInTheDocument()
+        expect(screen.getByText("Archie Duncombe")).toBeInTheDocument()
+    })
 
+    test('Clicking on the delete mail button without any selected email should have no effect', async () => {
+        expect(screen.getByText("Bambie Petera")).toBeInTheDocument()
+        const selectBambieCheckbox = screen.getByText("Bambie Petera").parentElement?.firstElementChild
+    })
 
 })
